b272a24c2781a822449aea5b64a6fbded9934274
Sam Guo <gmg903417@gmail.com>
2021-03-30 09:31:49 +0800

    Add quiz component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import NavBar from "./components/header/NavBar";
 import "./App.scss";
+import Quiz from "./components/quiz/Quiz";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <NavBar />
+        <Quiz />
         {/* <Catalog /> */}
       </header>
     </div>
